c7071396693ed5cd7b30c59c4e9beeeefd501ee0
SalvadorLimones <szocsbencekemence@gmail.com>
2023-09-08 15:54:12 +0200

    add order page

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import Store from "./services/Store.js";
 import API from "./services/API.js";
 import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
+
 //Link my menu components
 import { MenuPage } from "./components/MenuPage.js";
 import { DetailsPage } from "./components/DetailsPage.js";
 import { OrderPage } from "./components/OrderPage.js";
 import ProductItem from "./components/ProductItem.js";
+import CartItem from "./components/CartItem.js";
 
 window.app = {};
 app.store = Store;
--- services/Order.js
+++ services/Order.js
@@ -14,6 +14,6 @@ export async function addToCart(id) {
   }
 }
 
-export function removeFromCart() {
+export function removeFromCart(id) {
   app.store.cart = app.store.cart.filter((p) => p.product.id != id);
 }
